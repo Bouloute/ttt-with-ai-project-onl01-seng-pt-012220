e5105fff413a33f0af02b93da8002a59643827ba
Bouloute <annaick1995@gmail.com>
2020-03-10 17:23:34 -0400

    Computer and Player created. Computer plays randomly

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,9 @@
-class Computer
-
+module Players
+    class Computer < Player
+        def move(board)
+            "#{rand(8) +1}"
+            #Should check if cell is empty
+            #Should check if there is a winnig combo it can complete/block
+        end
+    end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,3 +1,8 @@
-class Human
-
+module Players
+    class Human < Player
+        def move(board)
+            puts "Please enter 1-9"
+            gets
+        end
+    end
 end
